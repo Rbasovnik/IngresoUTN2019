c88df143e271a79eb9f1b0a5184657e9b7717076
Rbasovnik <47338120+Rbasovnik@users.noreply.github.com>
2019-02-13 20:24:15 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -3,9 +3,13 @@ function Mostrar()
 //tomo la edad  
 var edad = parseInt(document.getElementById("edad").value);
 
-if(edad == 13,14,15,16,17){
+if (edad >=13 && edad <=17){
     alert("Es adolescente");
 }
+
+//if ( !(edad <13 || edad >17) ){
+    //alert("Es adolescente");
+//}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
+    var edad
 
+    edad = parseFloat(document.getElementById("edad").value);
+
+    if(edad >= 13 && edad <= 17){
+        alert("Es adolescente");
+    }
 
 
 
